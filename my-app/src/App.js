ed1db89449e32a4ffd399b6203e92c318dc4eeed
Induwara Hasaranga <induhasaranga@gmail.com>
2024-12-01 23:21:24 +0530

    npm

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <h1>Welcome to the Website</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
