a571cf9b0ee9fe8a033ee2581223c21e592d0d42
Kaveesha Lasith <90321578+shklasith@users.noreply.github.com>
2024-12-02 00:44:45 +0530

    Describe your changes

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -1,4 +1,3 @@
-// Navbar.js
 import React, { useState } from 'react';
 import './Navbar.css';
 
@@ -11,16 +10,24 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-logo">MyWebsite</div>
-      <button className="menu-toggle" onClick={toggleMenu}>
-        ☰
-      </button>
-      <ul className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#products">Products</a></li>
-      </ul>
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <img
+            src={require('./Resources/Untitled design (25).png')}
+            alt="Logo"
+            className="logo-image"
+          />
+        </div>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#products">Products</a></li>
+        </ul>
+      </div>
     </nav>
   );
 };
